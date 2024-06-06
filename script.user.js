d817e97d9b31ab55aa319504fc638711a854bb6e
Jan <47220014+Jan-Fcloud@users.noreply.github.com>
2024-06-06 20:18:34 +0200

    Userscript and answer json

---
+++ script.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         W101 Trivia
+// @namespace    https://github.com/Jan-Fcloud/W101-TriviaAnswers
+// @version      1.0
+// @description  Highlight the correct answer in the Wizard101 Trivia game
+// @author       Jan-FCloud
+// @match        https://www.wizard101.com/quiz/trivia/game*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Fetch and Parse the JSON from https://raw.githubusercontent.com/Jan-Fcloud/W101-TriviaAnswers/main/answers.json
+
+    $.getJSON("https://raw.githubusercontent.com/Jan-Fcloud/W101-TriviaAnswers/main/answers.json", function (data) {
+        console.log(data);
+        // Next get the Quiz title from the website url which is after "https://www.wizard101.com/quiz/trivia/game/"
+        let quizTitle = window.location.href.split("https://www.wizard101.com/quiz/trivia/game/")[1];
+
+        // remove every minus sign from the quiz title
+        quizTitle = quizTitle.replace(/-/g, " ");
+
+        // remove the word trivia from the quiz title
+        quizTitle = quizTitle.replace("trivia", "");
+
+        // Change all starts of words in title to uppercase
+        quizTitle = quizTitle.replace(/\b\w/g, l => l.toUpperCase());
+
+        // Remove any spaces at the end of the quiz title
+        quizTitle = quizTitle.trim();
+
+        // Get the trivia question from the div .quizQuestion
+        let question = $(".quizQuestion").text();
+
+        let answer = "";
+
+        // Find the question in the quizData
+        data[quizTitle].forEach(qElement => {
+            if (qElement[0].toUpperCase() === question.toUpperCase() || qElement[0].toUpperCase() === question.toUpperCase() + "?"){
+                console.log(qElement);
+
+                // Find the found answer in the answers on the website
+                let answers = $(".answerText");
+                console.log(answers);
+
+                answers.each(function (index, element) {
+                    var elementText = $(element).text().trim();
+                    console.log(elementText);
+                    if (elementText === qElement[1].trim()) {
+                        console.log("Found answer");
+                        $(element).css("background-color", "green");
+
+                        return true;
+                    } else {
+                        console.log("Not answer");
+                        console.log("qElement: " + qElement[1]);
+                        console.log("element: " + elementText);
+                    }
+                });
+
+            }
+        });
+    });
+
+
+})();
